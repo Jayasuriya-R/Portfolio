d23e736166eabf10a5b7a7a15989da7cc775baf2
Jayasuriya-R <suryaabi3801@gmail.com>
2025-05-16 22:23:08 +0530

    deisgned the portfolio home page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="flex justify-center" >
-      <div className="w-3/12 mt-9 p-3 font-bold flex justify-between bg-white border border-gray-200 rounded-full shadow-lg ">
+      <div className="w-3/12 mt-9 p-3 font-bold flex justify-between bg-white border border-gray-200 rounded-full shadow-lg hover:border-teal-500">
         <span className="hover:text-teal-500 cursor-pointer pl-2">Home</span>
         <span className="hover:text-teal-500 cursor-pointer">About</span>
         <span className="hover:text-teal-500 cursor-pointer">Projects</span>
